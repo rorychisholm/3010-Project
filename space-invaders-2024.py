11c41b560f90848d08b14b85d2b4b9c61a3a0a9e
rorychisholm <rchisholm@rogers.com>
2024-03-22 15:36:57 -0400

    basic pygame functionality added
    found outline from https://gist.github.com/MatthewJA/7544830

--- space-invaders-2024.py
+++ space-invaders-2024.py
@@ -5,3 +5,32 @@ William Rory Chisholm - 100560820
 ~ Space Invaders 2024 ~
 3010U - Simulation and Modling Project
 """
+
+import sys
+ 
+import pygame
+from pygame.locals import *
+ 
+pygame.init()
+ 
+fps = 60
+fpsClock = pygame.time.Clock()
+ 
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+ 
+# Game loop.
+while True:
+  screen.fill((0, 0, 0))
+  
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+  
+  # Update.
+  
+  # Draw.
+  
+  pygame.display.flip()
+  fpsClock.tick(fps)
